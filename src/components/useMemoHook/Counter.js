8e5efb1bb2b485811e59829bd7e45389f75c7234
visingh <visingh@ingenuitygaming.com>
2024-07-30 00:58:28 +0530

    use Memo

--- src/components/useMemoHook/Counter.js
+++ src/components/useMemoHook/Counter.js
@@ -1,31 +1,32 @@
-import React, {useState, useMemo} from 'react';
+import React, { useMemo, useState } from "react";
 
-function Counter() {
-  const [counterOne, setCounterOne]=useState(0);
-  const [counterTwo, setCounterTwo]=useState(0);
-  
-  const incrementCounterOne = () => {
-    setCounterOne(counterOne + 1)
+const Counter = () => {
+  const [counterOne, setCounterOne] = useState(0);
+  const [counterTwo, setCounterTwo] = useState(0);
+
+  const IncrementCountOne = () => {
+    console.log(">>>>>>>>IncrementCountOne>>>>>>>>>>")
+    setCounterOne(counterOne + 1);
+  }
+  const IncrementCountTwo = () => {
+    console.log(">>>>>>>>IncrementCountTwo>>>>>>>>>>")
+    setCounterTwo(counterTwo + 1);
   }
 
-  const incrementCounterTwo = () => {
-    setCounterTwo(counterTwo+1)
-  }
-  
-  const isEven = useMemo(() =>{
+  const CheckEven = useMemo(() => {
     let i = 0;
-    while(i < 2000000000) i++;
-    return counterOne % 2 === 0 ? ' Even': ' Odd'
-  }, [counterOne]);
+    while (i<2000000000) i++
+    return (counterOne%2 === 0 ? 'Even' : 'Odd');
+  },[counterOne]);
 
   return (
     <>
       <div>
-        <button onClick={incrementCounterOne} >CountOne {counterOne}</button>
-        <span>{isEven}</span>
+        <button onClick={IncrementCountOne}>Count {counterOne} times</button>
+        <span>{CheckEven}</span>
       </div>
       <div>
-        <button onClick={incrementCounterTwo} >CountTwo {counterTwo}</button>
+        <button onClick={IncrementCountTwo}>Count {counterTwo} times</button>
       </div>
     </>
   );
